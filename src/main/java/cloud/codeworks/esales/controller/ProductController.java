87f30da0a7ee5cea08b176bf49aa52f694d7fa73
santos.rafaelbs <santos.rafaelbs@192.168.1.16>
2017-03-15 13:38:37 -0300

    Configuring Bootstrap layout in the project

--- src/main/java/cloud/codeworks/esales/controller/ProductController.java
+++ src/main/java/cloud/codeworks/esales/controller/ProductController.java
@@ -1,8 +1,14 @@
 package cloud.codeworks.esales.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
+	
+	@RequestMapping("/product/new")
+	public String addProduct() {
+		return "product/add-product";
+	}
 
 }
